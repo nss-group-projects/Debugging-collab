cb0c0c63914a45caa2847bc053695a9437c813f4
Dakota Lambert <dakotalambertprog@gmail.com>
2022-10-06 16:24:23 -0400

    added bookForm.js

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -1,13 +1,34 @@
-const API = 'http://localhost:8088/'
-
 const applicationState = {
   books: [],
   genres: [],
   authors: [],
-}
+};
 
 // FETCH CALLS
-//^ Write fetch calls to get data
+//? Write fetch calls to get data
+export const fetchAuthors = () => {
+  return fetch("http://localhost:8088/authors")
+    .then((response) => response.json())
+    .then((yerboisAuthorData) => {
+      applicationState.authors = yerboisAuthorData;
+    });
+};
+
+export const fetchBooks = () => {
+  return fetch("http://localhost:8088/books")
+  .then((res) => res.json())
+  .then((dataFromResponse) => {
+    applicationState.books = dataFromResponse
+  })
+}
 
 // GETTER FUNCTIONS
-//^ Write getter functions to get application state
+//? Write getter functions to get application state
+
+export const getAuthors = () => {
+   return applicationState.authors.map((author) => ({...author}));
+};
+
+export const getBooks = () => {
+  return applicationState.books.map((book) => ({...book}))
+}
